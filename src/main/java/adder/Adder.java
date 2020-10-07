370046a8813597792ba0627564822a93565d17e1
Themba Mathebula <tpmathebula@gmail.com>
2020-10-07 19:19:26 +0200

    initial commit

---
+++ src/main/java/adder/Adder.java
@@ -0,0 +1,15 @@
+package adder;
+
+public class Adder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sum  = 5 + 4;
+		
+		System.out.println("The answer is : " + sum);	
+		
+		
+	}
+
+}
