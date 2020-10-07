75f13c13d789688fff971c5871b1fafb03f4b1f6
Themba Mathebula <tpmathebula@gmail.com>
2020-10-07 19:24:44 +0200

    added testcase

---
+++ src/test/java/adder/TestAdder.java
@@ -0,0 +1,15 @@
+package adder;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAdder {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertEquals("","");
+	}
+
+}
